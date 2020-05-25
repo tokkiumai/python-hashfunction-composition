dd9c97d3b92b39d5837b61fd19283a4f2b9222d8
theredbe <morelessequal@gmail.com>
2020-05-25 09:41:23 +0300

    feat: rot13 hash

---
+++ hashes/rot13.py
@@ -0,0 +1,15 @@
+import hashlib
+import codecs
+
+from utils import format_and_encode
+
+def hash_rot13(string, loops):
+  string_length = len(string)
+  
+  for _ in range(loops):
+    translated = codecs.encode(string, 'rot_13')
+    string = format_and_encode(translated)
+
+    string = hashlib.md5(string).hexdigest()
+  
+  return string[:string_length]
