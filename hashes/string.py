49c148fafb832c014c370134856baded4e023667
theredbe <morelessequal@gmail.com>
2020-05-25 07:37:24 +0300

    fix: hash string

--- hashes/string.py
+++ hashes/string.py
@@ -1,9 +1,10 @@
 import hashlib
 
 def hash_string(string, loops):
-  string = string.encode('utf-8')
+  string_length = len(string)
 
   for _ in range(1, loops):
-    string = hashlib.md5(string).hexdigest().encode('utf-8')
+    string = ' '.join(format(ord(x), 'b') for x in string).encode('utf-8')
+    string = hashlib.md5(string).hexdigest()
 
-  return string.decode('utf-8')
+  return string[:string_length]
